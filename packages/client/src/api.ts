093b0c2dc7f4efcd5df64d189eda414132a8ef4a
Collin Driscoll <provinite@gmail.com>
2022-04-09 17:36:15 -0500

    dummy 2

--- packages/client/src/api.ts
+++ packages/client/src/api.ts
@@ -1,5 +1,5 @@
 import { GraphQLClient } from "graphql-request";
 import { getSdk } from "./generated/graphql";
-//
+
 const client = new GraphQLClient("http://localhost:3000/");
 export const api = getSdk(client);
