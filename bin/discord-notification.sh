1348fd69bbd2ec2e9df8cdac45e0ef843061a2ab
Collin Driscoll <provinite@gmail.com>
2023-08-10 02:28:55 -0500

    mb now

--- bin/discord-notification.sh
+++ bin/discord-notification.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-FORCE_COLOR=0 ./bin/latest-changelogs.sh | jq -Rsa "{content: .}" | curl -H "Content-Type: application/json" -d @- "$WEBHOOK_URL"
+FORCE_COLOR=0 ./bin/latest-changelogs.sh | jq -Rs "{content: .}" | curl -H "Content-Type: application/json" -d @- "$WEBHOOK_URL"
