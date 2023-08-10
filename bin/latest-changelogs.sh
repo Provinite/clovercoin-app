d4bbcb9f584804882753f2d0962904f862207377
Collin Driscoll <provinite@gmail.com>
2023-08-10 01:52:38 -0500

    test push 2

--- bin/latest-changelogs.sh
+++ bin/latest-changelogs.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-yarn workspaces foreach --exclude clovercoin-app exec ../../bin/changelog.sh
+# pushd ./packages/api
+# ../../bin/changelog.sh
+# popd
+# pushd ./packages/client
+# ../../bin/changelog.sh
+# popd
+
+yarn workspaces foreach --exclude clovercoin-app exec ../../bin/changelog.sh | sed "s/^.*YN0000:\s*//g"
