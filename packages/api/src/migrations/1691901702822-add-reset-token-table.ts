550a9514138650aabd2f733ff7f1fb1dc482665b
Collin Driscoll <provinite@gmail.com>
2023-08-12 23:43:02 -0500

    add reset token table migration

---
+++ packages/api/src/migrations/1691901702822-add-reset-token-table.ts
@@ -0,0 +1,27 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AddResetTokenTable1691901702822 implements MigrationInterface {
+  name = "AddResetTokenTable1691901702822";
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "reset_token" ("id" uuid NOT NULL, "accountId" uuid NOT NULL, "claimedAt" TIMESTAMP WITH TIME ZONE, "revokedAt" boolean, CONSTRAINT "PK_93e1171b4a87d2d0478295f1a99" PRIMARY KEY ("id"))`
+    );
+    await queryRunner.query(
+      `CREATE UNIQUE INDEX "unique_reset_token_valid_per_account" ON "reset_token" ("accountId", "revokedAt", "claimedAt") WHERE "revokedAt" IS NULL and "claimedAt" IS NULL`
+    );
+    await queryRunner.query(
+      `ALTER TABLE "reset_token" ADD CONSTRAINT "FK_3fbb045fd52d7556cec0fbbbb7b" FOREIGN KEY ("accountId") REFERENCES "account"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "reset_token" DROP CONSTRAINT "FK_3fbb045fd52d7556cec0fbbbb7b"`
+    );
+    await queryRunner.query(
+      `DROP INDEX "public"."unique_reset_token_valid_per_account"`
+    );
+    await queryRunner.query(`DROP TABLE "reset_token"`);
+  }
+}
