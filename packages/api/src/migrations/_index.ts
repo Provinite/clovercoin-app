283d1c20ac20a5f0d5039889fa0c6acbb1edfb8e
Collin Driscoll <provinite@gmail.com>
2023-08-12 23:41:01 -0500

    remove leftover migration reference

--- packages/api/src/migrations/_index.ts
+++ packages/api/src/migrations/_index.ts
@@ -3,7 +3,6 @@ import { ChangeCritterTraitsToJsonb1690779921209 } from "./1690779921209-change-
 import { AddCritterTraitValuesGinIndex1691043318922 } from "./1691043318922-add-critter-trait-values-gin-index.js";
 import { AddInviteCodes1691795618473 } from "./1691795618473-add-invite-codes.js";
 import { AddInviteCodeCheckConstraint1691808450017 } from "./1691808450017-add-invite-code-check-constraint.js";
-import { AddResetTokenTable1691898872701 } from "./1691898872701-add-reset-token-table.js";
 
 export const migrationsArray = [
   initializeDb1690172455074,
@@ -11,5 +10,4 @@ export const migrationsArray = [
   AddCritterTraitValuesGinIndex1691043318922,
   AddInviteCodes1691795618473,
   AddInviteCodeCheckConstraint1691808450017,
-  AddResetTokenTable1691898872701,
 ];
