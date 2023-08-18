47bbbe2b851b02b8183aa9ed3099008416863c20
Collin Driscoll <provinite@gmail.com>
2023-08-17 22:16:40 -0500

    wip

--- bin/discord-notification.sh
+++ bin/discord-notification.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 export YARN_ENABLE_COLORS=false
-./bin/latest-changelogs.sh | jq -Rs "{content: .}" | curl -H "Content-Type: application/json" -d @- "$WEBHOOK_URL"
+git diff "$NX_BASE..HEAD" --name-only |
+  grep CHANGELOG.md |
+  xargs dirname |
+  sed 's/$/\/package.json/' |
+  xargs jq '.name ' |
+  xargs -I {} yarn workspace {} exec $(pwd)/bin/changelog.sh
+
+# ./bin/latest-changelogs.sh | jq -Rs "{content: .}" #| curl -H "Content-Type: application/json" -d @- "$WEBHOOK_URL"
