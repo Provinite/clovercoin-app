2181e13f48088c74bc5a52c59b0fa73ff708fdbe
Collin Driscoll <provinite@gmail.com>
2023-08-18 15:20:18 -0500

    fix discord notification erroring if no changelog

--- bin/discord-notification.sh
+++ bin/discord-notification.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-NX_BASE=3b1947de70fee1de2e300150e4171968aa91c29c
 export YARN_ENABLE_COLORS=false
 git diff "$NX_BASE..HEAD" --name-only |
   grep CHANGELOG.md |
-  xargs dirname |
+  xargs -r dirname |
   sed 's/$/\/package.json/' |
   xargs jq '.name ' |
-  xargs -I {} yarn workspace {} exec $(pwd)/bin/changelog.sh
+  xargs -I {} yarn workspace {} exec "$(pwd)/bin/changelog.sh"
 
 # ./bin/latest-changelogs.sh | jq -Rs "{content: .}" #| curl -H "Content-Type: application/json" -d @- "$WEBHOOK_URL"
