4cc0a679c7163edb09dd00dcd86341eebb20694e
Collin Driscoll <provinite@gmail.com>
2023-08-21 20:12:50 -0500

    fix(bin/discord-notification.sh): fix discord notification formatting and functionality

--- bin/discord-notification.sh
+++ bin/discord-notification.sh
@@ -9,11 +9,11 @@ contents=$(git diff $NX_BASE..HEAD \
  sed -e 's/^--[^/]\+[/]\(.*\)CHANGELOG.md/jq -r .name "\1package.json" | sed "s\/^\/# \/g"/eg'
 )
 if [ -n "$contents" ]; then
-  echo \
-    "# New Release in $CC_ENV_NAME"$'\n' \
-    "A new version of the clovercoin app has been pushed to the **$CC_ENV_NAME** site."$'\n' \
-    "Here is a summary of the latest changes!"$'\n\n' \
-    "$contents" |
-    jq -Rs "{content: .}" # | 
+  printf $'%s\n' \
+      "# New Release in $CC_ENV_NAME" \
+      "A new version of the clovercoin app has been pushed to the **$CC_ENV_NAME** site." \
+      "Here is a summary of the latest changes!"$'\n'
+      "$contents" |
+    jq -Rs "{content: .}"  | 
     curl -H "Content-Type: application/json" -d @- "$WEBHOOK_URL"
 fi
