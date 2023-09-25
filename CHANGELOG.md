eeb1e920800bba113228793d44cc01c3d2e000c0
Collin Driscoll <provinite@gmail.com>
2023-09-24 22:36:04 -0500

    fix discord notification

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 1.2.1
+
+- Fixed a bug that prevented the release discord notification from being sent
+
 # 1.2.0
 
 - Adds an SES SMTP VPC endpoint to the application's VPCs
--- bin/discord-notification.sh
+++ bin/discord-notification.sh
@@ -8,11 +8,12 @@ contents=$(git diff $NX_BASE..HEAD \
   sed -e '/^\(@@\|[+][+]\|index \|diff --git\).*$/d' |
   sed -e 's/^--[^/]\+[/]\(.*\)CHANGELOG.md/jq -r .name "\1package.json" | sed "s\/^\/# \/g"/eg'
 )
+
 if [ -n "$contents" ]; then
-  printf $'%s\n' \
+  printf '%s\n' \
       "# New Release in $CC_ENV_NAME" \
       "A new version of the clovercoin app has been pushed to the **$CC_ENV_NAME** site." \
-      "Here is a summary of the latest changes!"$'\n'
+      "Here is a summary of the latest changes!" \
       "$contents" |
     jq -Rs "{content: .}"  | 
     curl -H "Content-Type: application/json" -d @- "$WEBHOOK_URL"
