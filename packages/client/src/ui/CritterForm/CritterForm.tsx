24f687b3d6ce2dbe9c46753873f48b330eb0e387
Collin Driscoll <provinite@gmail.com>
2024-04-20 13:51:11 -0500

    fix(critter-form): show helpful error message when no variants are defined for species. Fix #38

--- packages/client/src/ui/CritterForm/CritterForm.tsx
+++ packages/client/src/ui/CritterForm/CritterForm.tsx
@@ -91,7 +91,12 @@ export const CritterForm: FC<CritterFormProps> = ({
           onChange={(e) => {
             setVariantId(e.target.value || null);
           }}
-          helperText="The critter's variant determines what traits and values are available."
+          error={species.variants.length === 0}
+          helperText={`${
+            species.variants.length
+              ? "The critter's variant determines what traits and values are available."
+              : "This species does not have any variants configured. Contact species owner."
+          }`}
         >
           <MenuItem value={""}>Choose a variant</MenuItem>
           {species.variants.map(({ id, name }) => (
