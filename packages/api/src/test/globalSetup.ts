16eb32fee943feb3c00c7d8e0f14b26de55a3355
Collin Driscoll <provinite@gmail.com>
2024-04-21 23:30:55 -0500

    fix(integration-tests): do not ever create db without cleaning up

--- packages/api/src/test/globalSetup.ts
+++ packages/api/src/test/globalSetup.ts
@@ -1,10 +1,47 @@
-import { shutdown } from "./preEnvSetup.js";
 import { setupTestClient } from "./testClient.js";
 import { beforeAll, afterAll } from "@jest/globals";
 import { registerAdminUser } from "./integration/register.js";
 import { setAdminUser } from "./integration/integrationCache.js";
 import { loginTestClient, logoutTestClient } from "./integration/login.js";
+import { dataSource } from "../db/dbConnection.js";
+import { createCloverCoinAppServer } from "../server.js";
+import { logger } from "../util/logger.js";
+import { createTestDb, dbName, dropTestDb } from "./integration/db.js";
+import { Server } from "http";
+
+let server: Server;
 beforeAll(async () => {
+  if (dataSource.isInitialized) {
+    throw new Error(
+      "Cannot configure CloverCoin application database since it is already initialized."
+    );
+  }
+
+  await createTestDb();
+
+  dataSource.setOptions({
+    database: dbName,
+  });
+
+  const { koa, rootContainer } = await createCloverCoinAppServer({
+    db: {},
+    schema: { emitFile: undefined },
+  });
+
+  await dataSource.runMigrations();
+
+  server = koa.listen(0);
+  const address = server.address();
+  if (!address || typeof address === "string") {
+    throw new Error("Unable to resolve koa port");
+  }
+  global.ccAppContainer = rootContainer;
+  global.ccAppAddress = address;
+  logger.info({
+    message: "Started application",
+    port: address.port,
+  });
+
   setupTestClient();
   const adminUser = await registerAdminUser();
 
@@ -20,5 +57,13 @@ beforeAll(async () => {
 afterAll(async () => {
   setAdminUser(undefined);
   logoutTestClient();
-  await shutdown();
+  // shutdown app server
+  await new Promise<void>((res, rej) =>
+    server.close((err) => (err ? rej(err) : res()))
+  );
+  await dataSource.destroy();
+  await dropTestDb();
+
+  global.ccAppAddress = undefined;
+  global.ccAppContainer = undefined;
 });
